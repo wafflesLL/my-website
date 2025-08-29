2559271dda378e6c91dc5fa70554298afd93df5b
wafflesLL <lloeffel@vols.utk.edu>
2025-08-29 15:28:56 +0000

    fixed gitsetup script

--- gitsetup.sh
+++ gitsetup.sh
@@ -12,9 +12,9 @@ set +a
 : "${GITHUB_EMAIL:?Missing GIT_USER_EMAIL in $ENV_FILE}"
 : "${GITHUB_PAT:?Missing GIT_PAT in $ENV_FILE}"
 
-git config --global user.email "$GITHUB_EMAIL"
-git config --global user.name "$GITHUB_NAME"
+git config --local user.email "$GITHUB_EMAIL"
+git config --local user.name "$GITHUB_NAME"
 
-git config --global credential.helper store
+git config --local credential.helper store
 
 printf 'protocol=https\nhost=github.com\nusername=%s\npassword=%s\n\n' "$GITHUB_NAME" "$GITHUB_PAT" | git credential approve
