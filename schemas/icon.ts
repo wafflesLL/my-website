253c9cd2693f98cb7845fb2ad34e372829522280
wafflesLL <lloeffel@vols.utk.edu>
2025-10-19 13:47:49 -0400

    fixed build problems

--- schemas/icon.ts
+++ schemas/icon.ts
@@ -1,34 +1,6 @@
 import { z } from "zod";
 
-export const IconSchema = z.object({
-  skills: z.array([
-    z.object({
-      name: z.string().min(1),
-      alt: z.string().optional(),
-      image: z
-        .string()
-        .min(1)
-        .refine(
-          (s) => s.startsWith("/images/") && s.endsWith(".webp"),
-          { message: "contentPath must be /images/*.webp" }
-        ),
-      description: z.string().min(1),
-    }),
-  ]),
-  developertools: z.array([
-    z.object({
-      name: z.string().min(1),
-      alt: z.string().optional(),
-      image: z
-        .string()
-        .min(1)
-        .refine(
-          (s) => s.startsWith("/images/") && s.endsWith(".webp"),
-          { message: "contentPath must be /images/*.webp" }
-        ),
-      description: z.string().min(1),
-    }),
-  ]),
+const IconSchema = z.object({
 });
 
 export type IconRecord = z.infer<typeof IconSchema>; 
